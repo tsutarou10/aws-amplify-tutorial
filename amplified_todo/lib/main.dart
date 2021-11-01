12691f07c6419f98e1f45f9d4b948b56e4e09b80
Tatsuro Miyazaki <tatsumiya05@gmail.com>
2021-11-02 05:52:05 +0900

    Update main.dart

---
+++ amplified_todo/lib/main.dart
@@ -0,0 +1,185 @@
+// dart async library we will refer to when setting up real time updates
+import 'dart:async';
+
+// flutter and ui libraries
+import 'package:flutter/material.dart';
+
+import 'models/ModelProvider.dart';
+import 'models/Todo.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amplified Todo',
+      home: TodosPage(),
+    );
+  }
+}
+
+class TodosPage extends StatefulWidget {
+  @override
+  _TodosPageState createState() => _TodosPageState();
+}
+
+class _TodosPageState extends State<TodosPage> {
+  @override
+  void initState() {
+    // to be filled in a later step
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // to be filled in a later step
+    super.dispose();
+  }
+
+  Future<void> _initializeApp() async {
+    // to be filled in a later step
+  }
+
+  Future<void> _configureAmplify() async {
+    // to be filled in a later step
+  }
+
+  Future<void> _fetchTodos() async {
+    // to be filled in a later step
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Todo List'),
+      ),
+      body: Center(child: CircularProgressIndicator()),
+      // body: _isLoading
+      //     ? Center(child: CircularProgressIndicator())
+      //     : TodosList(todos: _todos),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddTodoForm()),
+          );
+        },
+        tooltip: 'Add Todo',
+        label: Row(
+          children: [Icon(Icons.add), Text('Add todo')],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class TodosList extends StatelessWidget {
+  final List<Todo> todos;
+
+  TodosList({required this.todos});
+
+  @override
+  Widget build(BuildContext context) {
+    return todos.length >= 1
+        ? ListView(
+            padding: EdgeInsets.all(8),
+            children: todos.map((todo) => TodoItem(todo: todo)).toList())
+        : Center(child: Text('Tap button below to add a todo!'));
+  }
+}
+
+class TodoItem extends StatelessWidget {
+  final double iconSize = 24.0;
+  final Todo todo;
+
+  TodoItem({required this.todo});
+
+  void _deleteTodo(BuildContext context) async {
+    // to be filled in a later step
+  }
+
+  Future<void> _toggleIsComplete() async {
+    // to be filled in a later step
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: () {
+          _toggleIsComplete();
+        },
+        onLongPress: () {
+          _deleteTodo(context);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(todo.name,
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  Text(todo.description ?? 'No description'),
+                ],
+              ),
+            ),
+            Icon(
+                todo.isComplete
+                    ? Icons.check_box
+                    : Icons.check_box_outline_blank,
+                size: iconSize),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class AddTodoForm extends StatefulWidget {
+  @override
+  _AddTodoFormState createState() => _AddTodoFormState();
+}
+
+class _AddTodoFormState extends State<AddTodoForm> {
+  final _nameController = TextEditingController();
+  final _descriptionController = TextEditingController();
+
+  Future<void> _saveTodo() async {
+    // to be filled in a later step
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Todo'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(filled: true, labelText: 'Name')),
+              TextFormField(
+                  controller: _descriptionController,
+                  decoration:
+                      InputDecoration(filled: true, labelText: 'Description')),
+              ElevatedButton(onPressed: _saveTodo, child: Text('Save'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
